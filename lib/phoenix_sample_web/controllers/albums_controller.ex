468ef704a6d04a6471ab296d72c77da2865577a3
Ben Kiboma <omayioben@gmail.com>
2022-09-21 23:02:50 +0300

    Add data to database

--- lib/phoenix_sample_web/controllers/albums_controller.ex
+++ lib/phoenix_sample_web/controllers/albums_controller.ex
@@ -1,7 +1,11 @@
 defmodule PhoenixSampleWeb.AlbumsController do
+  alias PhoenixSample.{Repo, Album} # <--- 1
   use PhoenixSampleWeb, :controller
+  import Ecto.Query # <--- 2
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html",
+    albums: Repo.all(from a in Album,
+    select: %{:name => a.name, :singer => a.singer, :tracks => a.track_count}) # <--- 3
   end
 end
